08578741aa521c8927d3dc6c65112387826524b1
Alex Tunstall <alexandrejamestunstall@gmail.com>
2021-01-31 17:06:59 +0000

    Add CI pipeline configuration
    For now, it's just a basic compile, run tests, publish docs.

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
+import System.Environment
 import Test.Tasty
     ( TestTree
     , Timeout
@@ -17,6 +18,18 @@ import Golden
 
 main :: IO ()
 main = do
+    -- Nix doesn't make it easy to run a test suite with arguments.
+    mxml <- lookupEnv "ANT_XML_PATH"
+    case mxml of
+        Nothing -> runTests
+        Just xml -> do
+            putStrLn
+                $ "The test report will be written to $ANT_XML_PATH = " <> xml
+            args <- getArgs
+            withArgs ("--xml" : xml : args) runTests
+
+runTests :: IO ()
+runTests = do
     -- Generating test reports may fail on Windows if the encoding isn't UTF-8
     setLocaleEncoding utf8
     tests >>= defaultMainWithIngredients (antXMLRunner : defaultIngredients)
